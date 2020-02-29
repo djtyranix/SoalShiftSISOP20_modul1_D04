ca8179a28b77384b1dddf0bbf72dc957aff94737
Michael Ricky <37214301+djtyranix@users.noreply.github.com>
2020-02-29 21:56:27 +0700

    Revisi Solal 1

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,14 +1,20 @@
+#!/bin/bash
+
 echo -e "\n1.a)"
 echo -e "Region dengan profit paling sedikit:"
-awk -F '[\t:]' 'FNR==1 {next}{a[$13]+=$21;min=a[$13]}END{for(i in a){if(min>a[i]){min=a[i];r=i}}print r}' Sample-Superstore.tsv
+region=$(awk -F '[\t:]' 'FNR==1 {next}{a[$13]+=$21;min=a[$13]}END{for(i in a) print a[i],i}' Sample-Superstore.tsv | sort -g -k 1 | awk '(NR==1) {print $2}')
+echo $region
 
 echo -e "\n1.b)"
 echo -e "2 State dengan profit paling sedikit:"
-awk -F '[\t:]' '{if($13 == "Central")a[$11]+=$21} END {for(i in a) print a[i],i}' Sample-Superstore.tsv | sort -gk1 | awk '(NR<=2) {print $2}'
+awk -F '[\t:]' -v region="$region" '{if($13 == region )a[$11]+=$21} END {for(i in a) print a[i]"="i}' Sample-Superstore.tsv | sort -g -k 1 > state.txt
+awk -F '=' '{print $2}' state.txt | head -2
 
 echo -e "\n1.c)"
-echo -e "10 produk yang memiliki profit paling sedikit pada State hasil poin b:"
-echo -e "\nTexas:"
-awk -F '[\t:]' '{if($11 == "Texas" ) a[$17]+=$21} END {for(i in a) print a[i],i}' Sample-Superstore.tsv | sort -gk1 | awk 'NR<=10 {for(i=2;i<NF;i++) printf "%s", $i OFS; printf "%s", $NF ORS}'
-echo -e "\nIllinois:"
-awk -F '[\t:]' '{if($11 == "Illinois" ) a[$17]+=$21} END {for(i in a) print a[i],i}' Sample-Superstore.tsv | sort -gk1 | awk 'NR<=10 {for(i=2;i<NF;i++) printf "%s", $i OFS; printf "%s", $NF ORS}'
+echo -e "10 produk yang memiliki profit paling sedikit:"
+state1=`awk -F '=' '(NR==1){print $2}' state.txt`
+state2=`awk -F '=' '(NR==2){print $2}' state.txt`
+echo -e "\n$state1:"
+awk -F '[\t:]' -v state1="$state1" '{if($11 == state1 ) a[$17]+=$21} END {for(i in a) print a[i],i}' Sample-Superstore.tsv | sort -g -k 1 | awk '(NR<=10){for(i=2;i<NF;i++) printf "%s", $i OFS; printf "%s", $NF ORS}'
+echo -e "\n$state2:"
+awk -F '[\t:]' -v state2="$state2" '{if($11 == state2 ) a[$17]+=$21} END {for(i in a) print a[i],i}' Sample-Superstore.tsv | sort -g -k 1 | awk '(NR<=10) {for(i=2;i<NF;i++) printf "%s", $i OFS; printf "%s", $NF ORS}'
