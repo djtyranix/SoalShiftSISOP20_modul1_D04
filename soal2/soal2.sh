164ce22db97c22605f6bf984345ccaac73331e1b
Michael Ricky <37214301+djtyranix@users.noreply.github.com>
2020-02-29 21:40:21 +0700

    Deleted unused chars

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -6,7 +6,7 @@ namafile_trim=$(echo $namafile | tr -dc 'a-zA-Z.')
 
 if [ $namafile == $namafile_trim ]
 then
-	cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 28 | head -n 1 | (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$) > $PWD/$namafile_trim
+	cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 28 | head -n 1 > $PWD/$namafile_trim
 else
 	echo -e "Nama file hanya boleh menggunakan huruf saja. Silahkan jalankan script kembali.\n"
 fi
