f27e56d6775c4cc8fcf8b2841e4c4102e20fb9f2
Michael Ricky <37214301+djtyranix@users.noreply.github.com>
2020-02-29 21:37:14 +0700

    Added IF Condition for non alphabetic-only input

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -2,6 +2,11 @@
 
 namafile=$1
 
-namafile_trim=$(echo $namafile | tr -d '0-9')
+namafile_trim=$(echo $namafile | tr -dc 'a-zA-Z.')
 
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 28 | head -n 1 > $PWD/$namafile_trim
+if [ $namafile == $namafile_trim ]
+then
+	cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 28 | head -n 1 | (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$) > $PWD/$namafile_trim
+else
+	echo -e "Nama file hanya boleh menggunakan huruf saja. Silahkan jalankan script kembali.\n"
+fi
